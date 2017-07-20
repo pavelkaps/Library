12b6b791387a21eb97f019eff0355a39e4a10660
Poberezhniy Pasha <kaps.pavel@gmail.com>
2017-07-20 16:26:53 +0300

    update Lesson 19

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,12 +6,12 @@ import { ArticlesPageComponent } from './components/articles-page/articles-page.
 import { Routes } from '@angular/router';
 
 const itemRoutes: Routes = [
-    { path: 'app', component: ArticlesPageComponent},
+    { path: '', component: ArticlesPageComponent},
     { path: 'about/:id', component: ArticleDetailComponent},
 ];
 
 export const appRoutes: Routes =[
     { path: 'auth', component: AuthorizationComponent },
-    { path: '', component: LibraryComponent, children: itemRoutes},
+    { path: 'app', component: LibraryComponent, children: itemRoutes},
     { path: '**', redirectTo: '/app' }
 ];
